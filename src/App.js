78acd37fdf06bf6bf3613a594b6b7641b6608d64
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-18 18:02:00 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="conatiner">
+      <div className="quantity">
+        {/* <a href="#" className = "quantity_minus" title="Decrement"><span> - </span></a> */}
+        <a className="quantity_minus" title="decrement"> - </a>
+        <input name ="quantity" type="text" className="qquantity_input" value="10"/>
+        <a className="quantity_plus" title="increment"> + </a>
+        {/* <a href ="#" className= "quantity_plus" title="INCREMENT"><span> + </span></a> */}
+      </div>
     </div>
   );
 }
