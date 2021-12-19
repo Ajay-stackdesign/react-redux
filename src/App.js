89a1036066f5410a8eb5942b09002fce9fd257ea
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-12-19 23:29:26 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux"
+import { incNumber, dexNumber } from "./actions/index";
+import "./App.css"
+
 
 function App() {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch()
   return (
     <div className="conatiner">
       <div className="quantity">
         {/* <a href="#" className = "quantity_minus" title="Decrement"><span> - </span></a> */}
-        <a className="quantity_minus" title="decrement"> - </a>
-        <input name ="quantity" type="text" className="qquantity_input" value="10"/>
-        <a className="quantity_plus" title="increment"> + </a>
+        <a className="quantity_minus" title="decrement" onClick={() => dispatch(dexNumber())}> -</a>
+        <input name ="quantity" type="text" className="qquantity_input" value={myState}/>
+        <a className="quantity_plus" title="increment" onClick={() => dispatch(incNumber())}>+</a>
         {/* <a href ="#" className= "quantity_plus" title="INCREMENT"><span> + </span></a> */}
       </div>
     </div>
